5c4c95060b23f6c00d3f88836b9b01e6dd02dc80
Pacome-DS <pacome.destreel@gmail.com>
2024-01-04 13:19:18 +0100

    nouveau commit

---
+++ js/2.js
@@ -0,0 +1,15 @@
+function test() {
+  var ok = function () {
+    var test = function () {
+      confirm("Confirmation requise pour 'test'");
+    };
+
+    if (confirm("e cafe...")) {
+      confirm("Perfavore :)");
+    } else {
+      confirm("Perfavore :)");
+    }
+  };
+
+  ok();
+}
